756db96cf5d25c5bff680f9bef0c5bf72f6d84a6
Your Name <mwaurarita2019@gmail.com>
2019-10-06 20:20:59 +0300

    Add creat user class

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import pyperclip
 import random,string
 from passord_lock import User,credentials
+def creat_user(user_name,password):
+        '''
+        Function to add a new user to our users_list
+        '''
+        new_User=User(user_name,password)
+        
+        return new_User
+def save_New_User(user_name):
+    '''    
