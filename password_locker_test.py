39628394f1baa2831914af82af9715af7b240c78
Your Name <mwaurarita2019@gmail.com>
2019-10-07 08:46:51 +0300

    Add a new user

--- password_locker_test.py
+++ password_locker_test.py
@@ -4,3 +4,7 @@ from password_locker import Users,Credentials  # Importing the Credentials class
 
 class TestUser(unittest.TestCase):
      '''
+     Test a class that defines test cases for the user class behavious
+     '''
+     self.new_user = Users("Rita mwaura","britney3755"
+                           
