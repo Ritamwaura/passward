660046932caec5ab8308d4030418663ffb6debab
Your Name <mwaurarita2019@gmail.com>
2019-10-07 16:59:10 +0300

    Add final changes to my project

---
+++ credential.py
@@ -0,0 +1,39 @@
+import string
+import pyperclip
+from random import choice
+
+
+class Credentials:
+    credential_list = []
+
+
+def __init__(self, account, password, account_password):
+
+    self.account = account
+    self.password = account_password
+    self.account_password = account_password
+
+
+def save_credential(self):
+    '''
+    save_credential method saves credentials objects into credential_list
+    '''
+
+    Credentials.credential_list.append(self)
+    '''
+    save_credential method saves credentials objects into credential_list
+    '''
+
+@classmethod
+def generate_password(cls):
+    '''
+    Method that generates a random alphanumeric password
+    '''
+
+    size = 9
+
+    alphanum = string.ascii_uppercase + string.digits + string.ascii_lowercase
+
+    password = ''.join(choice(alphanum) for num in range(size))
+
+    return password
---
+++ user.py
@@ -0,0 +1,80 @@
+import pyperclip 
+
+class User:
+    
+    user_list = []  
+
+    def __init__(self,first_name,last_name,name,password):
+ 
+
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = name
+        self.password = password
+
+        
+
+    def save_user(self):
+        
+
+        '''
+        save_contact method saves contact objects into contact_list
+        '''
+        
+        User.user_list.append(self)  
+    def delete_user(self):
+
+        User.user_list.remove(self)
+
+    @classmethod
+    def find_by_name(cls,name):
+
+
+        for user in cls.user_list:
+            if user.username == name:
+                return user
+            
+    @classmethod
+    def user_exist(cls,name):
+
+        for user in cls.user_list:
+            if user.username == name:
+                    return True
+
+        return False
+    
+    @classmethod
+    def display_users(cls):
+
+        return cls.user_list
+
+    @classmethod
+    def copy_password(cls,name):
+        user_found = User.find_by_name(name)
+        pyperclip.copy(user_found.password)
+        
+
+    @classmethod
+    def display_user(cls,password):
+
+        user_user_list = []
+
+        for user in cls.user_list:
+            if user.user_password == password:
+                user_user_list.append(password)
+
+        return user_user_list
+
+    @classmethod
+    def log_in(cls, name, password):
+
+    
+        for user in cls.user_list:
+            if user.username == name and user.password == password:
+                return credential.credential_list
+
+        return False
+
+        
+    
