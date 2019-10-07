3e71e5474e7775f46bd5d0afd900c39cf2305427
Your Name <mwaurarita2019@gmail.com>
2019-10-07 08:36:27 +0300

    Add  import pyperclip module

--- password_locker_test.py
+++ password_locker_test.py
@@ -1,9 +1,6 @@
 import unittest  # Importing the unittest module
 import pyperclip  # Importing the pyperclip module
-from password_locker import Users,Credentials  # Importing the Credentials class
+#from password_locker import Users,Credentials  # Importing the Credentials class
 
-class TestUser(unittest.TestCase):
-    '''Test class that defines test cases for the Users class behavious.
-    '''
-    def setUp(self):
-        '''
+#class TestUser(unittest.TestCase):
+ ####    '''
