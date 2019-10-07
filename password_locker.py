6f69b4bf9ab67af1c370531aa749b7d5b99fcfaf
Your Name <mwaurarita2019@gmail.com>
2019-10-07 11:25:51 +0300

    Add  creat an instance of user

--- password_locker.py
+++ password_locker.py
@@ -2,3 +2,9 @@ import pyperclip,random,string
 class Users:
     
     users_list=[] # created an empty users list where users will be appended
+  def __init__(self,u_Name, pswd):
+        '''
+        Created an instance of a user 
+        '''
+        self.u_Name = u_Name
+        self.pswd = pswd
