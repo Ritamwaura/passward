5adaa02ae032571a7e294b3a2d86703c42f6f699
Your Name <mwaurarita2019@gmail.com>
2019-10-07 08:37:04 +0300

    Add  import credential class

--- password_locker_test.py
+++ password_locker_test.py
@@ -1,6 +1,6 @@
 import unittest  # Importing the unittest module
 import pyperclip  # Importing the pyperclip module
-#from password_locker import Users,Credentials  # Importing the Credentials class
+from password_locker import Users,Credentials  # Importing the Credentials class
 
 #class TestUser(unittest.TestCase):
  ####    '''
