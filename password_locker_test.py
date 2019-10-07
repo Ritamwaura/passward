ea2cabf1cfd84075aa89e694b23ab70cf583f44c
Your Name <mwaurarita2019@gmail.com>
2019-10-07 08:50:58 +0300

    Add a creat initializing class

--- password_locker_test.py
+++ password_locker_test.py
@@ -7,4 +7,10 @@ class TestUser(unittest.TestCase):
      Test a class that defines test cases for the user class behavious
      '''
      self.new_user = Users("Rita mwaura","britney3755"
+                           self.new_user = Users("Rita mwaura", "britney3755")  # create user object
+
+    def test_init(self):
+        self.assertEqual(self.new_user.u_Name, "Rita mwaura")
+        self.assertEqual(self.new_user.pswd, "britney3755")
+
                            
