f9adf14fdcd93451319bf85ff1e8f1b02ce52270
Your Name <mwaurarita2019@gmail.com>
2019-10-07 11:36:42 +0300

    Add  a class to innitialize

--- password_locker.py
+++ password_locker.py
@@ -36,3 +36,7 @@ class Users:
             if(user.u_Name == u_Name and user.pswd == pswd):
                     active_user == user.u_Name
         return active_user
+     def __init__(self,account_name,user_name, password):
+            self.account_name = account_name
+        self.user_name = user_name
+        self.password = password
