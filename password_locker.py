9bc7d204e2030710dd01c54376b873573f531b11
Your Name <mwaurarita2019@gmail.com>
2019-10-07 11:29:55 +0300

    Add  a method to save users

--- password_locker.py
+++ password_locker.py
@@ -8,7 +8,13 @@ class Users:
         '''
         self.u_Name = u_Name
         self.pswd = pswd
- def save_user(self):
+  def save_user(self):
+            '''
+        save_user method saves users in our class Users inside the users_list
+        '''
+        Users.users_list.append(self)
+         
+  def save_user(self):
             '''
         save_user method saves users in our class Users inside the users_list
         '''
