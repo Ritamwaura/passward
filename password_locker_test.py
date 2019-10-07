7bfe605ed2d1405dd70442dd04ffc7ef36f68973
Your Name <mwaurarita2019@gmail.com>
2019-10-07 08:38:01 +0300

    Add unnitest class

--- password_locker_test.py
+++ password_locker_test.py
@@ -2,5 +2,5 @@ import unittest  # Importing the unittest module
 import pyperclip  # Importing the pyperclip module
 from password_locker import Users,Credentials  # Importing the Credentials class
 
-#class TestUser(unittest.TestCase):
- ####    '''
+class TestUser(unittest.TestCase):
+     '''
